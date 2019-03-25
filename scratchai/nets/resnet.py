9c621c121808555460a4117dbd3ac5951babb50a
Arunava <learningdroidarunava@gmail.com>
2019-03-25 21:22:20 +0530

    resnext working!

--- scratchai/nets/resnet.py
+++ scratchai/nets/resnet.py
@@ -6,8 +6,9 @@ from .blocks.bnconv import bnconv
 
 class Resnet(nn.Module):
   
-  def __init__(self, nc:int, layers:list, lconv:int=2, expansion:int=1, block:nn.Module=Resblock,
-                     s1_channels:int=64, conv_first=True, inplace=True):
+  def __init__(self, nc:int, layers:list, lconv:int=2, expansion:int=1, 
+               block:nn.Module=Resblock, s1_channels:int=64, conv_first=True,
+               inplace=True):
     '''
     The class that defines the ResNet module
     
@@ -30,26 +31,26 @@ class Resnet(nn.Module):
                                 stride=2,
                                 padding=1),
     
-              res_stage(block, inc=s1_channels,
-                              outc=s1_channels*expansion,
+              res_stage(block, ic=s1_channels,
+                              oc=s1_channels*expansion,
                               num_layers=layers[0],
                               stride=1, lconv=lconv,
                               conv_first=conv_first, inplace=inplace),
     
-              res_stage(block, inc=s1_channels*expansion,
-                              outc=s1_channels*expansion*2,
+              res_stage(block, ic=s1_channels*expansion,
+                              oc=s1_channels*expansion*2,
                               num_layers=layers[1], 
                               conv_first=conv_first, lconv=lconv,
                               inplace=inplace),
     
-              res_stage(block, inc=s1_channels*expansion*2,
-                              outc=s1_channels*expansion*4,
+              res_stage(block, ic=s1_channels*expansion*2,
+                              oc=s1_channels*expansion*4,
                               num_layers=layers[2], 
                               conv_first=conv_first, lconv=lconv,
                               inplace=inplace),
     
-              res_stage(block, inc=s1_channels*expansion*4,
-                              outc=s1_channels*expansion*8,
+              res_stage(block, ic=s1_channels*expansion*4,
+                              oc=s1_channels*expansion*8,
                               num_layers=layers[3], 
                               conv_first=conv_first, lconv=lconv,
                               inplace=inplace),
@@ -98,4 +99,39 @@ def resnet152(nc, **kwargs):
     kwargs['nc'] = nc
     kwargs['layers'] = [3, 8, 56, 3]
     kwargs['lconv'] = 3
+    return Resnet(**kwargs)
+
+def resnext18(nc, **kwargs):
+    kwargs['nc'] = nc
+    kwargs['layers'] = [2, 2, 2, 2]
+    kwargs['lconv'] = 2
+    kwargs['block'] = Resnext
+    return Resnet(**kwargs)
+
+def resnext34(nc, **kwargs):
+    kwargs['nc'] = nc
+    kwargs['layers'] = [3, 4, 6, 3]
+    kwargs['lconv'] = 2
+    kwargs['block'] = Resnext
+    return Resnet(**kwargs)
+
+def resnext50(nc, **kwargs):
+    kwargs['nc'] = nc
+    kwargs['layers'] = [3, 4, 6, 3]
+    kwargs['lconv'] = 3
+    kwargs['block'] = Resnext
+    return Resnet(**kwargs)
+
+def resnext101(nc, **kwargs):
+    kwargs['nc'] = nc
+    kwargs['layers'] = [3, 4, 23, 3]
+    kwargs['lconv'] = 3
+    kwargs['block'] = Resnext
+    return Resnet(**kwargs)
+
+def resnext152(nc, **kwargs):
+    kwargs['nc'] = nc
+    kwargs['layers'] = [3, 8, 56, 3]
+    kwargs['lconv'] = 3
+    kwargs['block'] = ResneXt
     return Resnet(**kwargs)
