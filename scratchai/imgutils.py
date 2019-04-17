89a25dc134db83067f1633d41102ef8de3bcde92
iarunava <learningdroidarunava@gmail.com>
2019-04-17 13:34:42 +0530

    Fixed mask_reg function and added function to mark points on image

--- scratchai/imgutils.py
+++ scratchai/imgutils.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
+import operator
 
 def thresh_img(img:np.ndarray, rgb, tcol:list=[0, 0, 0]):
     """
@@ -26,7 +28,8 @@ def thresh_img(img:np.ndarray, rgb, tcol:list=[0, 0, 0]):
     img[tidx] = tcol
     return img
 
-def mask_reg(img, pnts, reln, deg=1, tcol=[0, 0, 0]):
+def mask_reg(img, pnts, reln, deg:int=1, tcol:tuple=(0, 0, 0), 
+             locate:bool=False, invert:bool=False) -> np.ndarray:
     """
     Region Masking.
 
@@ -47,10 +50,15 @@ def mask_reg(img, pnts, reln, deg=1, tcol=[0, 0, 0]):
            Do note: that n1 corresponds to the n1th element in points.
     deg : int
           Degree of the fitting polynomial.
-    tcol : list
+    tcol : tuple of 3 elements
           A list containing the rgb values for the
           color which the mask needs to be filled with.
-          Defaults to [0, 0, 0]
+          Defaults to (0, 0, 0)
+    locate : bool
+             Denotes if to mark the points also in the image. 
+             Defaults to False.
+    invert : bool
+             Denotes whether to invert the final indexes. Defaults to False.
 
     Returns
     -------
@@ -63,21 +71,49 @@ def mask_reg(img, pnts, reln, deg=1, tcol=[0, 0, 0]):
         assert type(param) == list or type(param) == tuple
     
     img = np.copy(img)
-    h, w = img.shape
+    h, w = img.shape[1], img.shape[0]
+    ops = {'<' : operator.lt, '>' : operator.gt}
     
     # Fit the lines
     psolns = {}
-    for ii, p1, p2 in enumerate(reln):
-        soln = np.polyfit((p1[0], p2[0]), (p1[1], p2[1]), deg)
+    for ii, (p1, p2, rel) in enumerate(reln):
+        pnt1 = pnts[p1]; pnt2 = pnts[p2]
+        soln = list(np.polyfit((pnt1[0], pnt2[0]), (pnt1[1], pnt2[1]), deg))
+        soln.append(rel)
         psolns[ii] = soln
-    
+
     # Find the region inside the lines
     xx, yy = np.meshgrid(np.arange(h), np.arange(w))
-    reg_thresh = np.zeros_like(xx)
-    for _, val in psolns.items():
-        reg_thresh &= (yy > (xx*val[0] + val[1]))
-    
+    for ii, val in psolns.items():
+        # TODO Give option to choose btw bitwise_and and bitwise_or
+        reg_thresh = ops[val[2]](yy, (xx*val[0] + val[1])) if not ii else \
+                     reg_thresh & ops[val[2]](yy, (xx*val[0] + val[1]))
+            
     # Color pixels which are inside the region of interest
-    img[reg_thresh] = tcol
+    img[reg_thresh == False if invert else reg_thresh] = tcol
+    
+    return img if not locate else mark_pnt_on_img(img, pnts)
 
+def mark_pnt_on_img(img, pnts:list, col:tuple=(0, 255, 0)) -> np.ndarray:
+    """
+    Mark points on Image.
+
+    Arguments
+    ---------
+    img : np.ndarray - Shape like (H x W x 3)
+          The image on which the points need to be marked.
+    pnts : list of tuples
+           where each tuple is (x, y) a point to mark.
+    col : tuple of 3 elements
+          which are the rgb values for the color of the point.
+    Returns
+    -------
+    img : np.ndarray
+          With all the points marked on the image
+    """
+
+    img = np.copy(img)
+    for pnt in pnts:
+        cv2.circle(img, pnt, 10, col, -1)
     return img
+
