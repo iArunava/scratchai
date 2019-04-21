797ebd773a49c3a45250edb7d0ea6c73abab7f22
iarunava <learningdroidarunava@gmail.com>
2019-04-21 18:27:44 +0530

    Ongoing implementation of fgm

--- scratchai/attacks/utils.py
+++ scratchai/attacks/utils.py
@@ -1,4 +1,52 @@
 import torch
+import numpy as np
+
+
+def optimize_linear(grads, eps, ordr):
+  """
+  Solves for optimal input to a linear function under a norm constraint.
+
+  Arguments
+  ---------
+  grads : torch.Tensor
+           The gradients of the input.
+  eps : float
+        Scalar specifying the constraint region.
+  ordr : [np.inf, 1, 2] 
+         Order of norm constraint.
+
+  Returns
+  -------
+  opt_pert : torch.Tensor
+             Optimal Perturbation.
+  """
+
+  red_ind = list(range(1, len(grads.size())))
+  azdiv = torch.tensor(1e-12, dtype=grads.dtype, device=grads.device)
+
+  if ordr == np.inf:
+    opt_pert = torch.abs(grads)
+
+  elif ordr == 1:
+    abs_grad = torch.abs(grads)
+    sign = torch.sign(grads)
+    ori_shape = [1] * len(grads.size())
+    ori_shape[0] = grads.size(0)
+
+    max_abs_grad, _ = torch.max(abs_grad.view(grads.size(0), -1), 1)
+    max_mask = abs_grad.eq(max_abs_grad.view(ori_shape)).float()
+    num_ties = max_mask
+    for red_scalar in red_ind:
+      num_ties = torch.sum(num_ties, red_scalar, keepdims=True)
+    opt_pert = sign * max_mask / num_ties
+
+  elif ordr == 2:
+    # TODO
+    pass
+
+  scaled_pert = eps * opt_pert
+  return scaled_pert
+
 
 def clip_eta(eta, ord, eps):
     """
