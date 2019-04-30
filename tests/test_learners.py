bedfc610577fc796b1f8facf3b88fd113af4ac5a
iarunava <learningdroidarunava@gmail.com>
2019-04-30 10:31:33 +0530

    Updated with needed changes

--- tests/test_learners.py
+++ tests/test_learners.py
@@ -6,20 +6,20 @@ class TestLearners(unittest.TestCase):
     
   def test_clf_test(self):
     # Check if the function is implemented
-    utils.check_if_implemented(learner.clflearner, 'clf_test')
+    utils.check_if_implemented(learners.clflearner, 'clf_test')
 
   def test_clf_train(self):
     # Check if the function is implemented
-    utils.check_if_implemented(learner.clflearner, 'clf_train')
+    utils.check_if_implemented(learners.clflearner, 'clf_train')
 
   def test_clf_fit(self):
     # Check if the function is implemented
-    utils.check_if_implemented(learner.clflearner, 'clf_fit')
+    utils.check_if_implemented(learners.clflearner, 'clf_fit')
 
   def test_train_mnist(self):
     # Check if the function is implemented
-    utils.check_if_implemented(learner.clflearner, 'train_mnist')
+    utils.check_if_implemented(learners.clflearner, 'train_mnist')
 
   def test_adjust_lr(self):
     # Check if the function is implemented
-    utils.check_if_implemented(learner.clflearner, 'adjust_lr')
+    utils.check_if_implemented(learners.clflearner, 'adjust_lr')
