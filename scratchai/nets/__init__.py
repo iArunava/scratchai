24e2f939ae522cce1868f2f2d36d09bebd01c613
Arunava <learningdroidarunava@gmail.com>
2019-05-24 13:34:54 +0530

    Implementation of Vgg (#41)
    * Successfully implemented VGG
    
    * Added tests for VGG
    
    * updated vgg readme
    
    * Update README.md

--- scratchai/nets/__init__.py
+++ scratchai/nets/__init__.py
@@ -1,7 +1,11 @@
 from scratchai.nets.seg.enet import *
 from scratchai.nets.seg.unet import *
+
 from scratchai.nets.clf.resnet import *
+from scratchai.nets.clf.vgg import *
 from scratchai.nets.clf.lenet import *
 from scratchai.nets.clf.alexnet import *
+
 from scratchai.nets.style_transfer import *
+
 from scratchai.nets.common import *
--- scratchai/nets/clf/__init__.py
+++ scratchai/nets/clf/__init__.py
@@ -1,3 +1,4 @@
 from scratchai.nets.clf.lenet import *
 from scratchai.nets.clf.alexnet import *
 from scratchai.nets.clf.resnet import *
+from scratchai.nets.clf.vgg import *
