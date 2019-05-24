aa7b433d11eedafb876598a7f32ddb1e85994668
iarunava <learningdroidarunava@gmail.com>
2019-05-24 12:31:12 +0530

    Successfully implemented VGG

--- scratchai/nets/__init__.py
+++ scratchai/nets/__init__.py
@@ -1,6 +1,7 @@
 from scratchai.nets.seg.enet import *
 from scratchai.nets.seg.unet import *
 from scratchai.nets.clf.resnet import *
+from scratchai.nets.clf.vgg import *
 from scratchai.nets.clf.lenet import *
 from scratchai.nets.clf.alexnet import *
 from scratchai.nets.style_transfer import *
