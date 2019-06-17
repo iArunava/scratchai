cc9ad4a00fbfeef788323f54559e7da64e438e4c
iarunava <learningdroidarunava@gmail.com>
2019-06-17 09:22:46 +0530

    Added InterLayer to get intermediate layers of a network

--- scratchai/nets/common.py
+++ scratchai/nets/common.py
@@ -5,6 +5,23 @@ Contains some commonly used functions b/w all modules.
 import torch
 import torch.nn as nn
 
+from collections import OrderedDict
+
+class InterLayer(nn.Module):
+  def __init__(self, layer, return_layers):
+    super().__init__()
+    self.layer = layer
+    self.return_layers = return_layers
+  
+  def forward(self, x):
+    out = OrderedDict()
+    for name, layer in self.layer.named_children():
+      x = layer(x)
+      if name in self.return_layers:
+        out[self.return_layers[name]] = x
+    return out
+
+
 class Flatten(nn.Module):
   def forward(self, x):
     return x.reshape(x.size(0), -1)
