162c2bfd45bc7f650d9a5218650a957a3217cc14
iarunava <learningdroidarunava@gmail.com>
2019-07-01 16:01:48 +0530

    Updates

--- scratchai/nets/clf/vgg.py
+++ scratchai/nets/clf/vgg.py
@@ -58,7 +58,7 @@ class VGG(nn.Module):
     self.features = nn.Sequential(*features)
     self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
     self.classifier = nn.Sequential(Flatten(), *linear(512 * 7 * 7, 4096), 
-                                    *linear(4096, 4096), nn.Linear(4096, nc)]
+                                    *linear(4096, 4096), nn.Linear(4096, nc))
 
   def forward(self, x):
     x = self.features(x)
@@ -67,65 +67,52 @@ class VGG(nn.Module):
     return x
 
 
+
+def get_net(pretrained=True, **kwargs):
+  cust_nc = kwargs['nc'] if 'nc' in kwargs else None
+  kwargs['ic'] = 1; kwargs['nc'] = 10
+  net = VGG(**kwargs)
+  if pretrained:
+    return load_pretrained(net, urls.alexnet_mnist_url, 'alexnet_mnist', 
+                           nc=cust_nc, attr='classifier', inn=9216)
+  return net
+  
 # =============================================
 # VGG A 
 # =============================================
 def vgg11():
-  cust_nc = kwargs['nc'] if 'nc' in kwargs else None
   return VGG(lconf=[1, 1, 2, 2, 2], norm=False)
 
 def vgg11_bn():
-  cust_nc = kwargs['nc'] if 'nc' in kwargs else None
   return VGG(lconf=[1, 1, 2, 2, 2])
 
 # =============================================
 # VGG B
 # =============================================
 def vgg13():
-  cust_nc = kwargs['nc'] if 'nc' in kwargs else None
   return VGG(lconf=[2, 2, 2, 2, 2], norm=False)
-  if pretrained:
-    return load_pretrained(net, urls.alexnet_mnist_url, 'alexnet_mnist', 
-                           nc=cust_nc, attr='classifier', inn=9216)
-  return net
 
 def vgg13_bn():
-  cust_nc = kwargs['nc'] if 'nc' in kwargs else None
   return VGG(lconf=[2, 2, 2, 2, 2])
-  if pretrained:
-    return load_pretrained(net, urls.alexnet_mnist_url, 'alexnet_mnist', 
-                           nc=cust_nc, attr='classifier', inn=9216)
-  return net
 
 # =============================================
 # VGG D
 # =============================================
 def vgg16():
-  cust_nc = kwargs['nc'] if 'nc' in kwargs else None
   return VGG(lconf=[2, 2, 3, 3, 3], norm=False)
  
 def vgg16_bn():
-  cust_nc = kwargs['nc'] if 'nc' in kwargs else None
   return VGG(lconf=[2, 2, 3, 3, 3])
 
 # =============================================
 # VGG E
 # =============================================
 def vgg19():
-  cust_nc = kwargs['nc'] if 'nc' in kwargs else None
   return VGG(lconf=[2, 2, 4, 4, 4], norm=False)
   
 def vgg19_bn():
-  cust_nc = kwargs['nc'] if 'nc' in kwargs else None
   return VGG(lconf=[2, 2, 4, 4, 4])
 
 """
 def alexnet_mnist(pretrained=True, **kwargs):
-  cust_nc = kwargs['nc'] if 'nc' in kwargs else None
-  kwargs['ic'] = 1; kwargs['nc'] = 10
-  net = Alexnet(**kwargs)
-  if pretrained:
-    return load_pretrained(net, urls.alexnet_mnist_url, 'alexnet_mnist', 
-                           nc=cust_nc, attr='classifier', inn=9216)
-  return net
 """
