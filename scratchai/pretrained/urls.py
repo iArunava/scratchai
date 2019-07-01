67dfdef4d978db0aa2db39c03d18240f158bb848
iarunava <learningdroidarunava@gmail.com>
2019-07-01 09:37:25 +0530

    Added links / ids to vgg pretrained models

--- scratchai/pretrained/urls.py
+++ scratchai/pretrained/urls.py
@@ -1,13 +1,38 @@
+# Pretrained Models from the Lenet Family.
+lenet_mnist_url = 'https://www.dropbox.com/s/0ylrj6a8gx1iiuw/lenet_mnist.pth?dl=1'
+
+
+# Pretrained Models from Alexnet Family.
+alexnet_url = 'https://www.dropbox.com/s/uloyaz86xehf6cl/alexnet.pth?dl=1'
+alexnet_mnist_url = 'https://www.dropbox.com/s/hlejnkp1c8s6l2q/alexnet_mnist.pth?dl=1'
+
+
+# Pretrained Models from VGG Family
+# Missing Pretrained Models
+# - VGG13_BN
+# - VGG19_BN
+# NOTE: I checked the pretrained models in PyTorch
+# but it doesn't seem to work! So, I have not taken those.
+vgg11    = '1bx9rrTFqaJuMamh9zz3CTVLBXj4TFv9Y'
+vgg11_bn = '1A25RIXstbWJugflXMR9_FIi_7FQdrvJp'
+vgg13    = '1-1vlwL6h8M5rDb5mU0QDeUHTJMq1UFP6'
+vgg16    = '1a59iu1J2ahRfyhWKxcB3gd4spKP8ZOJA'
+vgg16_bn = '1Wa5LzQE8wIDA0x6NNkwxLfNqjKDj81-a'
+vgg19    = '1-0CuXiEHIe8p8F7UjR93fgFoaGnj7V_X'
+
+
+# Pretrained Models from the Resnet Family
 resnet18_url = 'https://www.dropbox.com/s/ln4i4547jvn5wr8/resnet18.pth?dl=1'
 resnet34_url = 'https://www.dropbox.com/s/2mxj66dqtfqss2c/resnet34.pth?dl=1'
 resnet50_url = 'https://www.dropbox.com/s/hinaadj4kj1rsl0/resnet50.pth?dl=1'
 resnet101_url = 'https://www.dropbox.com/s/11u40th2ygsz9u0/resnet101.pth?dl=1'
 resnet152_url = 'https://www.dropbox.com/s/9babg74hcgx1w9l/resnet152.pth?dl=1'
+
+
+# Real Time Style Transfer Pretrained Models
 elephant_skin_url = 'https://www.dropbox.com/s/xak3qtgy426nwig/elephant_skin%281%29.pth?dl=1'
 snake_skin_url = 'https://www.dropbox.com/s/98nmx8gcsce49zr/snake_skin.pth?dl=1'
-lenet_mnist_url = 'https://www.dropbox.com/s/0ylrj6a8gx1iiuw/lenet_mnist.pth?dl=1'
-alexnet_url = 'https://www.dropbox.com/s/uloyaz86xehf6cl/alexnet.pth?dl=1'
-alexnet_mnist_url = 'https://www.dropbox.com/s/hlejnkp1c8s6l2q/alexnet_mnist.pth?dl=1'
+
 
 # This is a test dataset which is very small in size and 
 # helps ensures things are working as they are supposed to
