8c4dc1bd60be31057d26c20f10b7cb53acd7b3de
iarunava <learningdroidarunava@gmail.com>
2019-07-05 20:48:57 +0530

    Added the full GoogLeNet

--- scratchai/nets/clf/googlenet.py
+++ scratchai/nets/clf/googlenet.py
@@ -7,10 +7,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from scratchai.nets.common import Debug
+from scratchai.nets.utils import get_net
+from scratchai.nets.common import Flatten
 
 
-__all__ = ['InceptionA', 'GoogLeNet']
+__all__ = ['InceptionB', 'googlenet']
 
 
 def conv(ic:int, oc:int, k, s, p):
@@ -40,16 +41,21 @@ class AuxClassifier(nn.Sequential):
                              nn.Linear(1024, nc))
                              
 
-class InceptionA(nn.Module):
-  def __init__(self, ic:int, oc:int):
+class InceptionB(nn.Module):
+  def __init__(self, ic:int, oc1x1:int, mc3x3, oc3x3, mc5x5, oc5x5, ocpool):
     super().__init__()
 
-    self.net1x1 = nn.Sequential(*conv(ic, oc, 1, 1, 0))
-    self.net3x3 = nn.Sequential(*conv(ic, oc, 1, 1, 0), *conv(ic, oc, 3, 1, 2))
-    self.net5x5 = nn.Sequential(*conv(ic, oc, 1, 1, 0), *conv(ic, oc, 5, 1, 2))
-    self.pool   = nn.Sequential(nn.MaxPool2d(ic), *conv(ic, oc, 1, 1, 0))
+    self.net1x1 = nn.Sequential(*conv(ic, oc1x1, 1, 1, 0))
 
-  def forward(self, x):
+    self.net3x3 = nn.Sequential(*conv(ic, mc3x3, 1, 1, 0), 
+                                *conv(mc3x3, oc3x3, 3, 1, 1))
+
+    self.net5x5 = nn.Sequential(*conv(ic, mc5x5, 1, 1, 0), 
+                                *conv(mc5x5, oc5x5, 5, 1, 2))
+
+    self.pool   = nn.Sequential(nn.MaxPool2d(3, 1, 1), 
+                                *conv(ic, ocpool, 1, 1, 0))
+def forward(self, x):
     x1 = self.net1x1(x)
     x2 = self.net3x3(x)
     x3 = self.net5x5(x)
@@ -60,17 +66,59 @@ class InceptionA(nn.Module):
     
 
 class GoogLeNet(nn.Module):
-  def __init__(self, nc:int, inception=InceptionA):
+  def __init__(self, nc:int=1000, inception=InceptionB):
     super().__init__()
 
-    self.conv1 = nn.Sequential(*conv(3, 64, 7, 2, 1), Debug(), 
+    self.conv1 = nn.Sequential(*conv(3, 64, 7, 2, 1),
                                 nn.MaxPool2d(3, 2))
 
-    self.conv2 = nn.Sequential(*conv(64, 192, 3, 1, 1), Debug(), 
+    self.conv2 = nn.Sequential(*conv(64, 192, 3, 1, 1),
                                 nn.MaxPool2d(3, 2))
 
-    self.inception_3a = inception(192, 256)
-    self.inception_3b = inception(256, 480)
+    self.inception3a = inception(192, 64, 96, 128, 16, 32, 32)
+    self.inception3b = inception(256, 128, 128, 192, 32, 96, 64)
+
+    self.inception4a = inception(480, 192, 96, 208, 16, 48, 64)
+    self.inception4b = inception(512, 160, 112, 224, 24, 64, 64)
+    self.inception4c = inception(512, 128, 128, 256, 24, 64, 64)
+    self.inception4d = inception(512, 112, 144, 288, 32,  64, 64)
+    self.inception4e = inception(528, 256, 160, 320, 32, 128, 128)
+
+    self.inception5a = inception(832, 256, 160, 320, 32, 128, 128)
+    self.inception5b = inception(832, 384, 192, 384, 48, 128, 128)
+    
+    self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+    self.classifier = nn.Sequential(nn.Dropout(0.4), nn.Linear(1024, nc))
+    
+    self.maxpool = nn.MaxPool2d(3, 1)
 
   def forward(self, x):
     x = self.conv1(x)
+    x = self.conv2(x)
+
+    x = self.inception3a(x)
+    x = self.inception3b(x)
+    
+    x = self.maxpool(x)
+
+    x = self.inception4a(x)
+    x = self.inception4b(x)
+    x = self.inception4c(x)
+    x = self.inception4d(x)
+    x = self.inception4e(x)
+
+    x = self.maxpool(x)
+
+    x = self.inception5a(x)
+    x = self.inception5b(x)
+    
+    x = self.avgpool(x)
+    x = Flatten()(x)
+    x = self.classifier(x)
+    return x
+
+
+def googlenet(pretrained=False, **kwargs):
+  return get_net(GoogLeNet, pretrained=pretrained, pretrain_url=None, 
+                 fname='googlenet', kwargs_net=kwargs, attr='classifier',
+                 inn=1024)
