281c3de4cc3bb8ea9694bb1244d67ddb449d90de
iarunava <learningdroidarunava@gmail.com>
2019-07-25 08:12:44 +0530

    Updates

--- scratchai/nets/clf/resnet.py
+++ scratchai/nets/clf/resnet.py
@@ -5,6 +5,7 @@ The Resnet
 import torch
 import torch.nn as nn
 import scratchai.pretrained.urls as urls
+import numpy as np
 
 from scratchai.utils import load_from_pth, load_pretrained
 
@@ -13,7 +14,8 @@ __all__ = ['resnet18_mnist', 'resnet18', 'resnet34', 'resnet50', 'resnet50',
            'resnet101', 'resnet152']
 
 
-def conv(ic:int, oc:int, ks:int=3, s:int=1, p:int=1, norm:nn.Module=nn.BatchNorm2d, act:bool=True):
+def conv(ic:int, oc:int, ks:int=3, s:int=1, d:int=1, p:int=1, norm:nn.Module=nn.BatchNorm2d, act:bool=True):
+  #p = d if d > 1 else p
   layers = [nn.Conv2d(ic, oc, kernel_size=ks, stride=s, padding=p, bias=not norm)]
   if norm: layers += [norm(oc)]
   if act: layers += [nn.ReLU(inplace=True)]
@@ -50,7 +52,7 @@ class resblock(nn.Module):
 
   def __init__(self, ic:int, oc:int=None, norm:nn.Module=nn.BatchNorm2d, 
         act:bool=True, dflag:bool=False, pratio:int=4, layers:int=2, 
-        btype:str='basic', fdown:bool=False):
+        btype:str='basic', fdown:bool=False, dilation:int=1):
     super().__init__()
     assert btype in ['basic', 'bottleneck']
     self.dflag = dflag
@@ -77,7 +79,8 @@ class resblock(nn.Module):
 
 
 def res_stage(block:nn.Module, ic:int, oc:int, num_layers:int, dflag:bool=True,
-              btype:str='basic', fdown:bool=False):
+              btype:str='basic', fdown:bool=False, dilation:int=1, 
+              downx:int=32):
 
   """
 
@@ -100,15 +103,26 @@ def res_stage(block:nn.Module, ic:int, oc:int, num_layers:int, dflag:bool=True,
   fdown : bool
           If true the side branch *must* contain a conv block,
           whether it performs downsampling or not. Defaults to False.
+  
+  dilaltion : int
+              The dilation to be applied to the the first conv of the branch_2b
+              of each resblock.
+
+  downx : int
+          How much times the input size is to be reduced.
+          So, if it is 8, and the input has a height and width of 224 then the 
+          output is supposed to be 224/8
 
   Returns
   -------
   layers : list
            A list containing all the nn.Module that is required for this layer.
   """
-  
-  layers = [block(ic, oc, dflag=dflag, btype=btype, fdown=fdown)]
-  layers += [block(oc, oc, btype=btype) for i in range (num_layers-1)]
+  print (ic, oc, 'j')
+  layers = [block(ic, oc, dflag=dflag, btype=btype, fdown=fdown, \
+                  dilation=dilation)]
+  layers += [block(oc, oc, btype=btype, dilation=dilation) \
+             for i in range (num_layers-1)]
   return layers
 
 
@@ -129,19 +143,49 @@ class Resnet(nn.Module):
   """
 
   def __init__(self, layers:list, nc:int=1000, lconv:int=2, ex:int=1, 
-       block:nn.Module=resblock, ic:int=3, oc1:int=64, **kwargs):
+       block:nn.Module=resblock, ic:int=3, oc1:int=64, dilate_last:int=0,
+       downx:int=32, **kwargs):
     super().__init__()
-
+    
+    """
     features = [*conv(ic, oc1, 7, 2, 3),
-              nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+              nn.MaxPool2d(3, 2, 1),
               *res_stage(block, oc1, oc1*ex, layers[0], dflag=False, **kwargs),
               *res_stage(block, oc1*ex, oc1*ex*2, layers[1], **kwargs),
               *res_stage(block, oc1*ex*2, oc1*ex*4, layers[2], **kwargs),
               *res_stage(block, oc1*ex*4, oc1*ex*8, layers[3], **kwargs)]
+    """
+
+    features = [*conv(ic, oc1, 7, 2, 3), nn.MaxPool2d(3, 2, 1)]
+    blocks = len(layers)
+    curr_dilation = 1
+    ps1_last = blocks - np.log2(downx)
+
+    for ii, layer in enumerate(layers):
+      if ii >= (blocks-ps1_last): pool_stride = 1
+      if ii >= (blocks-dilate_last): curr_dilation = dilation
+      dflag = True if ii == 0 else False
+      
+      #features += self._make_stage(ii, ex, layers, dflag, **kwargs)
+      #print (1<<(max(0, ii-1)), 1<<ii)
+      #print (1<<(ex*int(0<ii)), 1<<(ex*int(0<(ii+1))))
+      print (oc1*(1<<(ex*int(0<ii)))*(1<<max(0, ii-1)), oc1*(1<<(ex*int(0<(ii+1))))*(1<<(ii)))
+      features += res_stage(block, oc1*(1<<(ex*int(0<ii)))*(1<<max(0, ii-1)),  \
+                            oc1*(1<<(ex*int(0<(ii+1))))*(1<<(ii)), \
+                            layer, dflag=dflag, **kwargs)
+
+
     self.features = nn.Sequential(*features)
     self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-    self.fc = nn.Linear(512*ex, nc)
-
+    self.fc = nn.Linear(512*(1<<ex), nc)
+  
+  def _make_stage(self, ii, ex, layers, dflag, **kwargs):
+    # NOTE ex is expected to be a power of 2, of if the expansion is needed
+    # to be 4, then ex should be 2
+    return res_stage(block, oc1*(1<<(ex*int(0<ii)))*(1<<max(0, ii-1)),  \
+                     oc1*(1<<(ex*int(0<(ii+1))))*(1<<(ii)), \
+                     layers[ii], dflag=dflag, **kwargs)
+  
   def forward(self, x):
     bs = x.size(0)
     x = self.features(x)
--- scratchai/nets/clf/vgg.py
+++ scratchai/nets/clf/vgg.py
@@ -4,16 +4,19 @@ VGG Model
 
 import torch
 import torch.nn as nn
+import numpy as np
 
 from scratchai.nets.utils import get_net
 from scratchai.nets.common import Flatten
 from scratchai.pretrained import urls
 
-__all__ = ['VGG', 'vgg11', 'vgg11_dilated', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 
+__all__ = ['VGG', 'vgg11', 'vgg_dilated', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 
            'vgg16_bn', 'vgg19', 'vgg19_bn', 'vgg_block']
 
 
-def vgg_block(ic:int, oc:int, k:int=3, s:int=1, p:int=1, d:int=1, norm:bool=True):
+def vgg_block(ic:int, oc:int, k:int=3, s:int=1, d:int=1, norm:bool=True):
+  # NOTE This overwrites the padding
+  p = d
   layers = [nn.Conv2d(ic, oc, k, s, p, dilation=d, bias=True)]
   if norm: layers += [nn.BatchNorm2d(oc)]
   layers += [nn.ReLU(inplace=True)]
@@ -47,17 +50,26 @@ class VGG(nn.Module):
          Defaults to True.
   """
   def __init__(self, nc=1000, lconf:list=[1, 1, 2, 2, 2], ic:int=3, 
-               norm:bool=True, dilation:int=1):
+               norm:bool=True, dilation:int=1, downx:int=32, dilate_last:int=0):
     super().__init__()
     
     ic = ic; oc = 64
+    pad = 1
     features = []
-    for l in lconf:
-      features += vgg_block(ic, oc, d=dilation, norm=norm)
+    blocks = len(lconf)
+    pool_stride = 2
+    curr_dilation = 1
+    ps1_last = blocks - np.log2(downx)
+    for ii, l in enumerate(lconf):
+      if ii >= (blocks-ps1_last): pool_stride = 1
+      if ii >= (blocks-dilate_last): curr_dilation = dilation
+
+      features += vgg_block(ic, oc, d=curr_dilation, norm=norm)
       ic = oc
-      for _ in range(l-1): features += vgg_block(ic, oc, d=dilation, norm=norm)
+      for _ in range(l-1):
+        features += vgg_block(ic, oc, d=curr_dilation, norm=norm)
       oc *= 2 if oc*2 <= 512 else 1
-      features += [nn.MaxPool2d(2, 2)]
+      features += [nn.MaxPool2d(3, pool_stride, padding=1)]
     
     self.features = nn.Sequential(*features)
     self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
@@ -71,13 +83,16 @@ class VGG(nn.Module):
     return x
 
   
-def vgg11_dilated(pretrained=True, **kwargs):
-  kwargs['lconf'] = [1, 1, 2, 2, 2]
-  kwargs['norm']  = False
+# =======================================================================
+# VGG Dilated
+# =======================================================================
+def vgg_dilated(vgg='vgg11', pretrained=True, **kwargs):
   kwargs['dilation'] = 2
-  return get_net(VGG, pretrained=pretrained, pretrain_url=urls.vgg11, 
-                 fname='vgg11', kwargs_net=kwargs, attr='classifier',
-                 inn=25088)
+  kwargs['dilate_last'] = 1
+  kwargs['downx'] = 8
+  return globals()[vgg](pretrained=pretrained, **kwargs)
+
+
 # =======================================================================
 # VGG A 
 # =======================================================================
@@ -143,3 +158,6 @@ def vgg19_bn(pretrained=False, **kwargs):
   return get_net(VGG, pretrained=pretrained, pretrain_url=None,
                  fname='vgg19_bn', kwargs_net=kwargs, attr='classifier',
                  inn=25088)
+
+
+
